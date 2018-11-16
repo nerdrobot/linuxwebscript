f0b1545b39d98eaf3195880254466e01e2acbf1e
nerdrobot <raohassan9673@gmail.com>
2018-11-15 20:05:21 -0700

    redirects to directory, generates files, and runs smoothly!!

--- run.sh
+++ run.sh
@@ -2,5 +2,22 @@ cd /var/www/html
 sudo touch index.html
 sudo chmod 777 index.html
 
+echo "<html><head><title> Welcome to Linux Project! </title></head>
+<body><p><a href = "/var/www/html/A/A.html">This link will redirect you to another page </a> </p> </body>
+</html>" > index.html
+
+sudo mkdir A
+cd A
+
+sudo touch A.html
+chmod 777 A.html
+
+echo "<html>
+<head>
+<title>Successfully redirected!</title>
+</head>
+<body> You have been successfully redirected from a link!</body>
+</html>" > A.html
+
 
 
